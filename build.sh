b19205d7f6736eee5a9951eb657a93a858a95153
sewerganger <wanghan9423@outlook.com>
2021-04-08 14:06:48 +0800

    chore: 设置git action

--- build.sh
+++ build.sh
@@ -2,31 +2,22 @@
 
 set -ex
 
-# Used in ./configure.
-CMD=${1:-build_x86_64}
-TAG=${2:14.16.0}
-
-export CONFIGURE_OPTIONS="--without-npm --with-intl=small-icu --shared"
-
 download_and_extract() {
-  local FILENAME="v$TAG.tar.gz"
+  local FILENAME="v$NODE_VERSION.tar.gz"
 
   curl -L https://github.com/nodejs/node/archive/refs/tags/${FILENAME} > $FILENAME
   tar zxvf "$FILENAME"
-  cp android-configure node-$TAG/
+  cp android-configure node-$NODE_VERSION/
+  cp common.gypi node-$NODE_VERSION/
 }
 
-
-build-android() {
+build_android() {
   ./android-configure $ANDROID_NDK_HOME $ANDROID_ABI 23
-   make -j4
+  make -j $(getconf _NPROCESSORS_ONLN)
+
+  tree ./out
 }
 
-build-linux-x86-64() {
-  ./configure --dest-cpu x86_64 $CONFIGURE_OPTIONS
-  make -j4
-}
-
-# Run in subshell 
 download_and_extract > /dev/null
-(cd node-$TAG && $CMD)
+cd node-$NODE_VERSION
+build_android
