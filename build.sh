d816fe59c0fd156df24731ba7d795c6d227f70e8
sewerganger <wanghan9423@outlook.com>
2021-04-08 14:05:40 +0800

    save

---
+++ build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -ex
+
+# Used in ./configure.
+CMD=${1:-build_x86_64}
+TAG=${2:14.16.0}
+
+export CONFIGURE_OPTIONS="--without-npm --with-intl=small-icu --shared"
+
+download_and_extract() {
+  local FILENAME="v$TAG.tar.gz"
+
+  curl -L https://github.com/nodejs/node/archive/refs/tags/${FILENAME} > $FILENAME
+  tar zxvf "$FILENAME"
+  cp android-configure node-$TAG/
+}
+
+
+build-android() {
+  ./android-configure $ANDROID_NDK_HOME $ANDROID_ABI 23
+   make -j4
+}
+
+build-linux-x86-64() {
+  ./configure --dest-cpu x86_64 $CONFIGURE_OPTIONS
+  make -j4
+}
+
+# Run in subshell 
+download_and_extract > /dev/null
+(cd node-$TAG && $CMD)
